34e8487f5a6a530693ce25ee626b783ac812b7db
Ansh09-chawla <ansh.jhande@gmail.com>
2023-10-08 12:33:32 -0600

    uploadind Job Match system project

---
+++ server/src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+
+const {
+	LoginService,
+	RegisterService,
+	JobPostsService,
+	ApplyService,
+	OffersService,
+	ProfileService,
+	SearchService,
+} = require("./services");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/login", LoginService);
+app.use("/register", RegisterService);
+app.use("/job-posts", JobPostsService);
+app.use("/apply", ApplyService);
+app.use("/offers", OffersService);
+app.use("/profile", ProfileService);
+app.use("/search", SearchService);
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000");
+});
+
+module.exports = app;
